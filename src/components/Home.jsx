e50deba0f515c16c18833fef255ed7b9c3d8f792
John Navarro <alessandro7833@gmail.com>
2023-06-29 10:21:41 -0600

    create the component

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home Works</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Profile = () => {
+  return(
+    <View>
+      <Text>Profile Works</Text>
+    </View>
+  )
+}
+
+export default Profile
