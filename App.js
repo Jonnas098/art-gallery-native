76bf7665f637845f0b948cbd10fe83bbf3d71f93
John Navarro <alessandro7833@gmail.com>
2023-06-29 18:40:39 -0600

    first routes added

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Route, Routes } from 'react-router-native';
+import { NativeRouter, Route, Routes } from 'react-router-native';
 
 import Home from './src/components/Home'
+import Profile from './src/components/Profile'
 import Header from './src/components/Header';
 import ArtistView from './src/components/ArtistView';
 import Footer from './src/components/Footer';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <NativeRouter>
+      <SafeAreaView style={styles.container}>
         <Header/>
-        <ScrollView style={styles.scrollArea}>
-          <ArtistView/>      
-        </ScrollView>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/art' element={
+          <ScrollView style={styles.scrollArea}>
+            <ArtistView/>      
+          </ScrollView>
+          }/>
+        </Routes>
         <Footer/>
         <StatusBar style="dark" />
-    </SafeAreaView>
+      </SafeAreaView>
+    </NativeRouter>
   );
 }
 
