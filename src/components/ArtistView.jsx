1c4f22a1da8854b4732903237058390c7dc6809c
John Navarro <alessandro7833@gmail.com>
2023-06-30 15:01:59 -0600

    add refreshing, liked, backcount stated, onRefresh funct and update likedArt

--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useState, useEffect} from 'react';
+import {ScrollView, StyleSheet, Text, View, RefreshControl, Vibration} from 'react-native';
+import React, { useState, useEffect, useCallback} from 'react';
 import artService from '../services/artServices';
 import ArtWorkView from './ArtWorkView';
 
 const ArtistView = () => {
   const [artists, setArtists] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+  const [liked, setLiked] = useState(false)
+  const [backCount, setBackCount] = useState(0)
 
   useEffect(()=>{
     console.log("Effect");
@@ -14,49 +17,74 @@ const ArtistView = () => {
     });
   }, []);
 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true)
+    artService.getAll().then(arts => {
+      console.log("Promise fulfilled on refresh")
+      setArtists(arts)
+      setRefreshing(false)
+    })
+  }, [])
+
   const likeArt = (id, idArtWork) => {
     const artist = artists.findIndex(a => a.id === id)
     const artWork = artists[artist].obras.findIndex(o => o._id === idArtWork)
     const updatedArtWorks = [...artists]
 
-    updatedArtWorks[artist].obras[artWork].likes += 1
-    setArtists(updatedArtWorks)
+    setBackCount(backCount + 1)
+    setTimeout(()=> setBackCount(0), 500)
+    console.log(backCount)
 
-    artService.update(id, updatedArtWorks[artist])
-    .then(updated => {
-      console.log(updated);
-    })
+    if(backCount === 1) {
+      setBackCount(0)
+      updatedArtWorks[artist].obras[artWork].likes += 1
+      setArtists(updatedArtWorks)
+      artService.update(id, updatedArtWorks[artist])
+      .then(updated => {
+        console.log(updated);
+      })
+    }
+
+    Vibration.vibrate(10)
   };
 
-
   return (
-    <View style={styles.artistView}>
+    <ScrollView
+      style={styles.artistView}
+      refreshControl={
+      <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
       {artists.length === 0 ? <Text style={styles.authorName}>Loading...</Text> :
-      artists.map(element => (
-        <View key={element.id}>
-          <Text style={styles.authorName}>{element.nombre}</Text>
-          {element.obras.map(obra => (
-            <ArtWorkView
-              key={obra._id}
-              content={obra}
-              likeFunction={() => likeArt(element.id, obra._id)}
-            />
-          ))}
-        </View>
-      ))}
-    </View>
+        artists.map(element => (
+          <View key={element.id}>
+            <Text style={styles.authorName}>{element.nombre}</Text>
+            {element.obras.map(artWork => (
+              <ArtWorkView
+                key={artWork._id}
+                content={artWork}
+                liked={liked}
+                likeFunction={() => likeArt(element.id, artWork._id)}
+              />
+            ))}
+          </View>
+        ))}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   artistView: {
     width: '100%',
-    paddingHorizontal: 15
+    paddingHorizontal: 15,
+    marginBottom: 15
   },
   authorName: {
     textAlign: 'center',
     fontSize: 20,
+    fontFamily: 'serif',
     marginVertical: 10
+  },
+  contentContainer: {
+    //backgroundColor: 'powderblue',
   }
 })
 
