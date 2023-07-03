94c3c8a8e5b1edb37c8c592c108fa0d2d098f539
John Navarro <alessandro7833@gmail.com>
2023-07-03 12:35:25 -0600

    created liked and backCount states and testFun. change render method

--- src/components/ArtWorkView.jsx
+++ src/components/ArtWorkView.jsx
@@ -1,34 +1,69 @@
-import { TouchableHighlight, StyleSheet, Text, View, Image, Button } from 'react-native';
+import {TouchableHighlight, StyleSheet, Text, View, Image, Vibration} from 'react-native';
+import React, {useState} from "react/index";
+import artService from "../services/artServices";
 
 const Separator = () => <View style={styles.separator}/>
 
-const ArtWorkView = ({ likeFunction, content, liked }) => {
+const ArtWorkView = ({ content, arr }) => {
+  const [liked, setLiked] = useState(false)
+  const [backCount, setBackCount] = useState(0)
 
-  const label = liked ? "Dislike" : "like"
+  const label = liked ? '❤️' : '🖤'
+
+  const testFun = (id, idArtWork) => {
+    const artist = arr.findIndex(a => a.id === content.id)
+    const artWorks = arr[artist].obras.findIndex(o => o._id === idArtWork)
+    const updatedArts = [...arr]
+
+    setBackCount(backCount + 1)
+    setTimeout(()=> setBackCount(0), 500)
+
+    if(backCount === 1 && !liked) {
+      setBackCount(0)
+      setLiked(true)
+      updatedArts[artist].obras[artWorks].likes += 1
+      artService.update(id, updatedArts[artist])
+        .then(updated => {
+          console.log(updated);
+        })
+      Vibration.vibrate(12)
+    } else if (backCount === 1 && liked) {
+      setBackCount(0)
+      setLiked(false)
+      updatedArts[artist].obras[artWorks].likes -= 1
+      artService.update(id, updatedArts[artist])
+        .then(updated => {
+          console.log(updated);
+        })
+      Vibration.vibrate(12)
+    }
+  }
 
   return(
-    <View style={styles.artWorkView}>
-      <TouchableHighlight onPress={likeFunction}>
-        <Image
-          source={{ uri: content.img }}
-          style={styles.artWork}
-        />
-      </TouchableHighlight>
-      <Separator/>
-      <View style={styles.info}>
-        <Text style={styles.info.title}>Author:</Text>
-        <Text style={styles.info.content}>{content.author}</Text>
-        <Text style={styles.likes}>Likes: {content.likes}</Text>
+    content.obras.map(artWork => (
+      <View key={artWork._id} style={styles.artWorkView}>
+        <TouchableHighlight onPress={()=> {testFun(content.id, artWork._id)}}>
+          <Image
+            source={{ uri: artWork.img }}
+            style={styles.artWork}
+          />
+        </TouchableHighlight>
+        <Separator/>
+        <View style={styles.info}>
+          <Text style={styles.info.title}>Author:</Text>
+          <Text style={styles.info.content}>{artWork.author}</Text>
+          <Text style={styles.likes}>{label} {artWork.likes}</Text>
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.info.title}>Place:</Text>
+          <Text style={styles.info.content}>{artWork.place}</Text>
+        </View>
+        <View>
+          <Text style={styles.info.title}>Description:</Text>
+          <Text>{artWork.description}</Text>
+        </View>
       </View>
-      <View style={styles.info}>
-        <Text style={styles.info.title}>Place:</Text>
-        <Text style={styles.info.content}>{content.place}</Text>
-      </View>
-      <View>
-        <Text style={styles.info.title}>Description:</Text>
-        <Text>{content.description}</Text>
-      </View>
-    </View>
+    ))
   )
 }
 
